5f4f517e50d001948a26e28c1acd80112ad25d65
Pranjal Verma <pranjal.verma80@gmail.com>
2016-04-15 18:07:43 +0530

    navigation drawer

--- app/src/main/java/com/pranjals/nsit/jobtracker/HomeScreen.java
+++ app/src/main/java/com/pranjals/nsit/jobtracker/HomeScreen.java
@@ -2,10 +2,12 @@ package com.pranjals.nsit.jobtracker;
 
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -31,6 +33,19 @@ public class HomeScreen extends AppCompatActivity {
 
          drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
          menu = (RecyclerView)findViewById(R.id.list_slidermenu);
+        ActionBarDrawerToggle barDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.drawer_open,R.string.drawer_closed){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        drawerLayout.setDrawerListener(barDrawerToggle);
+        barDrawerToggle.syncState();
         ArrayList<String> menuItems = new ArrayList<>();
         menuItems.add("Orders");
         menuItems.add("Customers");
@@ -40,32 +55,36 @@ public class HomeScreen extends AppCompatActivity {
         adapter.setClickListener(new NavDrawerListAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
-                switch(position){
+                switch (position) {
 
                     case 1:
-                        Toast.makeText(HomeScreen.this,"Order List Activity launched",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(HomeScreen.this, "Order List Activity launched", Toast.LENGTH_SHORT).show();
                         drawerLayout.closeDrawer(menu);
                         break;
                     case 2:
-                        Toast.makeText(HomeScreen.this,"Customer List Activity launched",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(HomeScreen.this, "Customer List Activity launched", Toast.LENGTH_SHORT).show();
                         drawerLayout.closeDrawer(menu);
                         break;
                     case 3:
-                        Toast.makeText(HomeScreen.this,"Employer List Activity launched",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(HomeScreen.this, "Employer List Activity launched", Toast.LENGTH_SHORT).show();
                         drawerLayout.closeDrawer(menu);
                         break;
                     case 4:
-                        Toast.makeText(HomeScreen.this,"Settings Activity launched",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(HomeScreen.this, "Settings Activity launched", Toast.LENGTH_SHORT).show();
                         drawerLayout.closeDrawer(menu);
                         break;
                     default:
-                        Toast.makeText(HomeScreen.this,"Touched item unrecogonizable",Toast.LENGTH_SHORT).show();
-                           break;
+                        Toast.makeText(HomeScreen.this, "Touched item unrecogonizable", Toast.LENGTH_SHORT).show();
+                        break;
                 }
             }
         });
         menu.setAdapter(adapter);
         menu.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+
 
 //        getActionBar().setDisplayHomeAsUpEnabled(true);
 //        getActionBar().setHomeButtonEnabled(true);
@@ -84,4 +103,11 @@ public class HomeScreen extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==android.R.id.home){
+            drawerLayout.openDrawer(menu);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
